c0476cac1e4b738804a6679a7e21cfea266b9f9e
dirisala221 <saikrishna.dirisala@gmail.com>
2023-09-06 13:08:18 -0400

    Questions of the list using the While Loop.

---
+++ PracticeCodes/ListMethodsWhileLoop.py
@@ -0,0 +1,7 @@
+if __name__ == "__main__":
+    arrayValues = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    # TODO:- get the max value of the arrayValues using the while loop
+    # Your Code.
+    # TODO: - Get the sum of the arrayValues using the while loop
+    # Your code.
+    # TODO: - Get the minimum values of the arrayValues using the while loop
