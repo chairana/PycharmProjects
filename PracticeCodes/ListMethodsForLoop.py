c7581679129846073f54087acadba63242190f08
Chaitanya Krishna Dirisala <chairana1995@gmail.com>
2023-09-06 15:30:10 -0400

    solution for the max,min,sum functions using for loop

--- PracticeCodes/ListMethodsForLoop.py
+++ PracticeCodes/ListMethodsForLoop.py
@@ -1,7 +1,30 @@
 if __name__ == "__main__":
-    arrayValues = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    arrayValues = [22,200, 1000, 200, -1, 1, 0, 500, 255,2000]
+    # TODO: - write the code with out using the "min" and "max" methods.
     # TODO:- get the max value of the arrayValues using the for loop
-    # Your Code.
+    # Your Code
+
     # TODO: - Get the sum of the arrayValues using the for loop
     # Your code.
     # TODO: - Get the minimum values of the arrayValues using the for loop
+    # Your code
+    minvalue=arrayValues[0]
+    for x in arrayValues:
+        if x < minvalue:
+            minvalue=x
+    print(minvalue)
+
+    sum=0
+    for x in arrayValues:
+        sum=sum+x
+print(sum)
+
+
+
+
+
+
+
+
+
+
