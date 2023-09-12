ae59d8b7916b66c030578d076b4230041dca723d
dirisala221 <saikrishna.dirisala@gmail.com>
2023-09-11 20:04:37 -0400

    Class Example Questions.

---
+++ PracticeCodes/Classes/Vechile.py
@@ -0,0 +1,9 @@
+class Vechile:
+
+    #Todo:- Create the constructor which takes three variables(vechileType, Max Speed, Milage) as input
+    #Todo: - Store them accrodingly we can use them in other methods
+
+
+if __name__ == "__main__":
+    #Todo: - Create the class object variable
+    #Todo: - Print the vehileType, MaxSpeed and Milages using the object
