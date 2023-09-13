0e794d7fc3decfe95472fee5922e65853513e796
dirisala221 <saikrishna.dirisala@gmail.com>
2023-09-12 21:11:05 -0400

    Modifications required in the vechile class. Please update the file.

--- PracticeCodes/Classes/Vechile.py
+++ PracticeCodes/Classes/Vechile.py
@@ -15,5 +15,14 @@ if __name__ == "__main__":
     vehicleobj.getvehicletype()
     vehicleobj.getmilage()
 
-    #Todo: - Create the class object variable for Vechile
-    #Todo: - Print the vehileType, MaxSpeed and Milages using the object
+    """
+    Comment1:- Formate the file. There are lot of highlighting underlines showing in the code.
+    
+    Comment2: - The name of the arguments in  the constructor class are not in the camel format.
+    
+    Comment3: - The name of the function names are not in the camel format.
+    
+    comment4: - Name of the object in the line 14 is not in the cammel format.
+    
+    """
+
